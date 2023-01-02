aeda0fc932d0b4284122ec87e89b1758cb49d5ee
AvinashSingh <AvinashSingh@LAPTOP-J56B33P3>
2023-01-02 12:19:45 +0530

    cmmt

---
+++ src/pageobjects/MyPageObject.java
@@ -0,0 +1,16 @@
+package pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@SalesforcePage( title="My Page Object"                                
+               , connection="Admin1"
+               , lightningWebComponent="showToast", namespacePrefix = "breadwinner_qbo"
+     )             
+public class MyPageObject {
+	
+}
